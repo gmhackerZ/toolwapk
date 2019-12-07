494bceaba056aee90736ffc76a8f0f9da5f605de
gmhackerZ <58559757+gmhackerZ@users.noreply.github.com>
2019-12-07 11:41:26 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+#TOOLWARE
+
+สคริปนี้เป็นสคริปสำหรับการติดตั้ง สคริปอื่นเขียนโดย defalt
+
+โปรดเขียนคำสั่งตามนี้เพื่อใช้งานสคริป
+
+
+pkg upgrade
+
+pkg install python2
+
+pkg install git
+
+git clone https://github.com/MALW4R3/TOOLWARE
+
+ls
+
+cd TOOLWARE
+
+python2 TOOLWARE.PY
+
+เสร็จ
+โปรดติดตม  defalt th ด้วย
+CU Aagin
---
+++ toolmak.py
@@ -0,0 +1,284 @@
+import os, sys
+
+
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdhyso++++osyhdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMds/-`            `.:sdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy:`                    `:yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy-                          -yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/                              /mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:                                -mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/                                  :NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                                    oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.                                    `mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                                      oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-                                      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMN                                        mMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                                        yMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                                        oMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                                        oMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                                        sMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMm  mNmdy+:`                    `:+yhmNm` dMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM- dMMMMMMNy:                :yNMMMMMMm .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs oMMMMMMMMMh-            -hMMMMMMMMMs oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN-`dMMMMMMMMMNo          +NMMMMMMMMMd`-NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm..hMMMMMMMMMMs        oMMMMMMMMMMd..mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd.`+mMMMMMMMMMo      +MMMMMMMMMNo`.dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm- .+dMMMMMMMN:    -NMMMMMMMdo. -dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/  `-+ydmNMMd`   hMMNNdy+-`  :mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs`    `..-:-    ---..`    `oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh-                      .hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+`                   /mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy-                .yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmo.            .+mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmy:.      .:smMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmhsooshmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print " MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+print "                                                                           "
+print "                                                                           "
+print "                                                                           "
+print "                                                                           "
+print "                                                                           "
+print "                          _____ ___   ___  _ __        ___    ____  _____  "
+print "                         |_   _/ _ \ / _ \| |\ \      / / \  |  _ \| ____| "
+print "                           | || | | | | | | | \ \ /\ / / _ \ | |_) |  _|   "
+print "                           | || |_| | |_| | |__\ V  V / ___ \|  _ <| |___  "
+print "                           |_| \___/ \___/|_____\_/\_/_/   \_\_| \_\_____| "
+print "                                                                           "
+print "                                     tool by : DEFALT                      "
+print "                                     group : MALWARE                       "
+print "                                     youtube : defalt th                   "
+print "                              "
+print "     [01]>[DDOS]              "
+print "     [02]>[HACKCAMERA]        "
+print "     [03]>[TROJAN]            "
+print "     [04]>[EXPLOIT]           "
+print "     [05]>[CRACK]             "
+print "     [06]>[SERVER]            "
+print "                              "
+print "     [00]>[EXIT]              "
+A = raw_input("MTW==>")
+   
+if  A == "1" or A == "01":
+     print "         [01] hammer "
+     print "         [02] Planetwork-DDOS " 
+     print "         [03] DDos-Attack "     
+     print "         [00] back to main menu"
+    
+     DDOS = raw_input("MTW==> ")
+
+     if DDOS == "01" or DDOS == "1":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2 ")
+                        os.system ("git clone https://github.com/cyweb/hammer")
+                        os.system ("mv hammer /$HOME")
+                        os.system ("figlet Hammer | lolcat")
+                        os.system ("clear")
+                        os.system ("python2 TOOLWARE.py")
+     elif DDOS == "02" or DDOS == "2":
+                        os.system ("clear")
+                        os.system ("apt-get upgrade apt-get install python2")
+                        os.system ("clear")
+                        os.system ("git clone https://github.com/Hydra7/Planetwork-DDOS")
+                        os.system ("mv Planetwork-DDOS /$HOME")
+                        os.system ("clear")
+                        os.system ("python2 TOOLWARE.py")
+     elif DDOS == "03" or DDOS == "3":
+                        os.system ("clear")   
+                        os.system ("apt-get upgrade apt-get install python2")
+                        os.system ("clear")
+                        os.system ("git clone https://github.com/Ha3MrX/DDos-Attack")
+                        os.system ("cd DDos-Attack")
+                        os.system ("python2 ddos-attack.py")
+                        os.system ("clear")
+     elif DDOS == "00" or DDOS == "0":
+                        os.system ("python2 TOOLWARE.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")
+if A == "2" or A == "02":
+     print "         [01] saycheese              "
+     print "         [02] ipcs                   "
+     print "         [00] back to main menu      "
+
+
+     A = raw_input("MTW==> ")
+
+     if A == "01" or A == "1":
+                         os.system ("clear")
+                         os.system ("apt-get upgrade apt-get install openssh php")
+                         os.system ("git clone https://github.com/thelinuxchoice/saycheese")
+                         os.system ("mv saycheese /$HOME")
+                         os.system ("figlet saycheese | lolcat")
+                         os.system ("clear")
+                         os.system ("python2 TOOLWARE.py  ")
+     elif A == "02" or A == "1":
+                         os.system ("clear")
+                         os.system ("apt-get upgrade ")
+                         os.system ("pip2 install mechanize requests")
+                         os.system ("git clone https://github.com/kancotdiq/ipcs")
+                         os.system ("mv ipcs /$HOME")
+                         os.system ("figlet IPCS | lolcat")
+                         os.system ("clear")
+                         os.system ("python2 TOOLWARE.py")
+     elif A == "00" or A == "0":
+                         os.system ("python2 TOOLWARE.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")
+if  A == "3" or A == "03":
+     print "         [01] A-Rat                "
+     print "         [02] Metasploit-Framework "
+     print "         [03] msfconsole           "
+     print "      "
+     print "         [00] back to main menu    "
+
+     TROJAN = raw_input("MTW==> ")
+
+     if TROJAN == "01" or TROJAN == "1":
+                        os.system ("clear")
+                        os.system ("apt-get install python python2 ")
+                        os.system ("git clone https://github.com/Xi4u7/A-Rat")
+                        os.system ("mv A-Rat /$HOME")
+                        os.system ("clear")
+                        os.system ("python2 TOOLWARE.py")
+     elif TROJAN == "02" or TROJAN == "2":
+                        os.system ("clear")
+                        os.system ("apt-get upgrade ")
+                        os.system ("apt-get install ruby")
+                        os.system ("pkg install figlet")
+                        os.system ("gem install lolcat")
+                        os.system ("clear")
+                        os.system ("figlet METASPLOIT | lolcat")
+                        os.system ("git clone https://github.com/rapid7/metasploit-framework")
+                        os.system ("mv metasploit-framework /$HOME")
+                        os.system ("clear")
+                        os.system ("python2 TOOLWARE.py")
+     elif TROJAN == "03" or TROJAN == "3":
+                        os.system ("apt-get upgrade")
+                        os.system ("apt-get install openssh")
+                        os.system ("apt-get install ruby")
+                        os.system ("pkg install figlet")
+                        os.system ("gem install lolcat")
+                        os.system ("clear")
+                        os.system ("figlet msfconsole | lolcat")
+                        os.system ("clear")
+                        os.system ("pkg install unstable")
+                        os.system ("pkg install metasploit")
+                        os.system ("clear")
+                        os.system ("python2 TOOLWARE.py")
+     elif TROJAN == "00" or TROJAN == "0":
+                        os.system ("python2 TOOLWARE.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")          
+if A == "4" or A == "04":
+     print "         [01] websploit         "
+     print "         [02] Routersploit      "
+     print "          "
+     print "         [00] back to main menu "
+
+     EXPLOIT = raw_input("MTW==> ") 
+
+     if EXPLOIT == "01" or EXPLOIT == "1":
+                       os.system ("apt-get upgrade")
+                       os.system ("pip install scapy")
+                       os.system ("figlet WEBSPLOIT | lolcat")
+                       os.system ("git clone https://github.com/websploit/websploit")
+                       os.system ("mv websploit /$HOME")
+                       os.system ("python2 TOOLWARE.py")
+     elif EXPLOIT == "02" or EXPLOIT == "2":
+                       os.system ("apt-get upgrade ")
+                       os.system ("git clone https://github.com/threat9/routersploit")
+                       os.system ("figlet Routersploit | lolcat")
+                       os.system ("clear")
+                       os.system ("mv routersploit /$HOME")
+                       os.system ("python2 TOOLWARE.py")
+     elif EXPLOIT == "00" or EXPLOIT == "0":
+                        os.system ("python2 TOOLWARE.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")
+if A == "5" or A == "05":
+     print "         [01] CRACKWARE         "
+     print "         [02] Brute_Force       "
+     print "                                "
+     print "         [00] back to main menu "
+ 
+     CRACK = raw_input("MTW==> ")
+  
+     if CRACK == "01" or CRACK == "1":
+                       os.system ("apt-get upgrade")
+                       os.system ("pkg install figlet")
+                       os.system ("pkg install ruby")
+                       os.system ("gem install lolcat")
+                       os.system ("figlet crackware | lolcat")
+                       os.system ("git clone https://github.com/MALW4R3/CRACKWARE")
+                       os.system ("mv CRACKWARE /$HOME")
+                       os.system ("python2 TOOLWARE.py")
+     elif CRACK == "02" or CRACK == "2":
+                       os.system ("apt-get upgrade ")
+                       os.system ("pkg install figlet")
+                       os.system ("pkg install ruby")
+                       os.system ("gem install lolcat")
+                       os.system ("figlet Brute_Force | lolcat")
+                       os.system ("git clone https://github.com/Matrix07ksa/Brute_Force")
+                       os.system ("mv Brute_Force /$HOME")
+                       os.system ("clear")
+                       os.system ("python2 TOOLWARE.py")
+     elif CRACK == "00" or CRACK == "0":
+                        os.system ("python2 TOOLWARE.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")
+if A == "6" or A == "06":
+     print "         [01] MyServer          "
+     print "         [02] server.sh         "
+     print "                                "
+     print "         [00] back to main menu "
+
+     SERVER = raw_input("MTW==> ")
+
+     if SERVER == "01" or SERVER == "1":
+                       os.system ("apt-get upgrade")
+                       os.system ("pkg install figlet")
+                       os.system ("pkg install ruby")
+                       os.system ("gem install lolcat")
+                       os.system ("figlet MyServer | lolcat")
+                       os.system ("git clone https://github.com/Rajkumrdusad/MyServer")
+                       os.system ("mv MyServer /$HOME")
+                       os.system ("python2 TOOLWARE.py")
+     elif SERVER == "02" or SERVER == "2":
+                       os.system ("apt-get upgrade ")
+                       os.system ("pkg install figlet")
+                       os.system ("pkg install curl")
+                       os.system ("pkg install ruby")
+                       os.system ("gem install lolcat")
+                       os.system ("figlet SERVER | lolcat")
+                       os.system ("curl -LO https://raw.githubusercontent.com/sachin175638/server1/master/server.sh")
+                       os.system ("sh server.sh")
+                       os.system ("clear")
+                       os.system ("python2 TOOLWARE.py")
+     elif SERVER == "00" or SERVER == "0":
+                        os.system ("python2 TOOLWARE.py")
+     else:
+                        print "\nERROR: Wrong Input"
+                        os.system ("python2 TOOLWARE.py")
